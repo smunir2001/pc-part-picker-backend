c0d05bf8cacacef135dfdd645bf1e14e4c2f7bfb
Sami Munir <smunir2001@gmail.com>
2023-04-20 00:53:09 -0400

    adding data/Users.txt & functionality for writing to Users.txt

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.io.*;
+import java.nio.Buffer;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Welcome to PC Utopia");
         System.out.println("--------------------\n");
 
         Address tempAddress = new Address("123 Main St.", "Somerset", "New Jersey", "08873", "United States");
         User tempUser = new User("John", "Doe", "johndoe123@email.com", "johnDoe123", tempAddress);
         System.out.println(tempUser);
+
+        FileWriter fileWriter = new FileWriter("data/Users.txt", true);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        PrintWriter printWriter = new PrintWriter(bufferedWriter);
+        printWriter.println(tempUser.toString());
+        printWriter.close();
     }
 }
